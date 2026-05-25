230e3fd7c170b101d51c4ef5f1ea033b329b3a24
Ibaad Shaikh <ishaik4520@gmail.com>
2026-05-25 14:25:16 -0400

    Delete RecipeRecommender/Recipe directory

--- RecipeRecommender/Recipe/config.py
+++
@@ -1,5 +0,0 @@
-RECIPES_PATH = "./input/df_recipes.csv"
-PARSED_PATH = "./input/df_parsed.csv"
-
-TFIDF_ENCODING_PATH = "models/tfidf.pkl"
-TFIDF_MODEL_PATH = "models/tfidf.pkl"
--- RecipeRecommender/Recipe/setup.sh
+++
@@ -1,8 +0,0 @@
-mkdir -p ~/.streamlit/
-echo "[general]
-" > ~/.streamlit/credentials.toml
-echo "[server]
-headless = true
-port = $PORT
-enableCORS = false
-" > ~/.streamlit/config.toml
